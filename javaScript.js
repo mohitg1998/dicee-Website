c53412f561d5ddcfd766b39ca18c2125db2fb29d
Mohit Gupta <59468087+mohitg1998@users.noreply.github.com>
2020-11-30 10:22:48 +0530

    files added

---
+++ javaScript.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.floor((Math.random() * 6) + 1); //1-6
+var randomNumber2 = Math.floor((Math.random() * 6) + 1); //1-6
+
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🏁 Player 1 Wins!";
+}
+else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins 🏁!";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw!";
+}
+
+
+
+/* MadeByMohit */
